aa3a82c4598d71631ccdba1d8b87fa7342ab95eb
wazari972 <wazari972@373d8ef0-abc8-4151-9680-7fa2cb6b1372>
2010-07-13 20:19:18 +0000

    Generic importer plugin
    git-svn-id: http://subversion.assembla.com/svn/WebAlbums@158 373d8ef0-abc8-4151-9680-7fa2cb6b1372

--- WebAlbums3/trunk/WebAlbums3-iDAO/src/net/wazari/dao/entity/Photo.java
+++ WebAlbums3/trunk/WebAlbums3-iDAO/src/net/wazari/dao/entity/Photo.java
@@ -7,7 +7,7 @@ package net.wazari.dao.entity;
 
 import java.io.Serializable;
 import java.util.List;
-import net.wazari.common.plugins.Metadata;
+import net.wazari.common.plugins.Importer.Metadata;
 import net.wazari.dao.entity.facades.EntityWithId;
 import net.wazari.dao.entity.facades.PhotoOrAlbum;
 
