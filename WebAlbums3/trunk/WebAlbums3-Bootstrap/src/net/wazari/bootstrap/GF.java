9eb8c510bbf1b1e598e607602c80ac210fb0e5ed
wazari972 <wazari972@373d8ef0-abc8-4151-9680-7fa2cb6b1372>
2010-07-16 16:00:26 +0000

    big steps towards Embedded Glassfish!
    git-svn-id: http://subversion.assembla.com/svn/WebAlbums@181 373d8ef0-abc8-4151-9680-7fa2cb6b1372

--- WebAlbums3/trunk/WebAlbums3-Bootstrap/src/net/wazari/bootstrap/GF.java
+++ WebAlbums3/trunk/WebAlbums3-Bootstrap/src/net/wazari/bootstrap/GF.java
@@ -13,9 +13,11 @@ package net.wazari.bootstrap;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.glassfish.api.ActionReport;
+import org.glassfish.api.ActionReport.MessagePart;
 import org.glassfish.api.admin.CommandRunner;
 import org.glassfish.api.admin.ParameterMap;
 import org.glassfish.api.embedded.ContainerBuilder;
@@ -34,17 +36,20 @@ public class GF {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws LifecycleException, IOException, InterruptedException {
-        Server server = startServer(80) ;
+        Server server = startServer(8081) ;
+        try {
+            //createUsers(server);
+            createJDBC(server);
 
-        createUsers(server);
-        createJDBC(server);
-        server.stop() ;
-        if (true) return ;
-        EmbeddedDeployer deployer = server.getDeployer();
-        log.info("Deploying ");
-        String appName = deployer.deploy(new File(PATH_EAR, "WebAlbums3-ea.ear"), null);
-        log.log(Level.INFO, "Deployed {0}", appName);
-
+            //if (true) return ;
+            EmbeddedDeployer deployer = server.getDeployer();
+            log.info("Deploying ");
+            String appName = deployer.deploy(new File(PATH_EAR, "WebAlbums3-ea.ear"), null);
+            log.log(Level.INFO, "Deployed {0}", appName);
+        } catch (Throwable t) {
+            t.printStackTrace() ;
+            server.stop();
+        }
     }
 
     private static Server startServer(int port) throws LifecycleException, IOException {
@@ -60,23 +65,35 @@ public class GF {
 
     }
 
-    private static void asAdmin(Server server, String command, ParameterMap params) {
+    private static List<MessagePart> asAdmin(Server server, String command, ParameterMap params) throws Throwable {
         CommandRunner runner = server.getHabitat().getComponent(CommandRunner.class);
         ActionReport report = server.getHabitat().getComponent(ActionReport.class);
         log.log(Level.INFO, "Invok {0} {1}", new Object[]{command, params});
-        runner.getCommandInvocation(command, report).parameters(params).execute();
-        log.info("command invoked ");
+        
+        log.info("command invoked");
+        if (params == null) {
+            runner.getCommandInvocation(command, report).execute();
+        } else {
+            runner.getCommandInvocation(command, report).parameters(params).execute();
+        }
+        if (report.hasFailures()){
+            report.writeReport(System.out);
+            throw report.getFailureCause() ;
+        }
+
+        return report.getTopMessagePart().getChildren() ;
     }
 
-    private static void createUsers(Server server) {
+    private static void createUsers(Server server) throws Throwable {
         ParameterMap params = new ParameterMap();
-        params.add("groups", "Admin, Manager");
+        params.add("groups", "Admin");
+        params.add("groups", "Manager");
         params.add("username", "Kevin");
         params.add("userpassword", "pass");
         asAdmin(server, "create-file-user", params);
     }
 
-    private static void createJDBC(Server server) {
+    private static void createJDBC(Server server) throws Throwable {
         ParameterMap params = new ParameterMap();
         params.add("datasourceclassname", "com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
         params.add("restype", "javax.sql.DataSource");
@@ -91,23 +108,33 @@ public class GF {
         params.add("lazyconnectionassociation", "false");
         params.add("associatewiththread", "false");
         params.add("matchconnections", "false");
-        params.add("ping", "false");
+        params.add("ping", "true");
         params.add("pooling", "true");
 
         params.add("wrapjdbcobjects", "false");
 
         params.add("property", "URL=jdbc\\:mysql\\://127.0.0.1/WebAlbums_test:User=wazari972:Password=ijaheb");
-        params.add("", "cpMySQLWebAlbumsTest");
+        params.add("DEFAULT", "cpMySQLWebAlbumsTest");
         asAdmin(server, "create-jdbc-connection-pool", params);
 
+
+        for (MessagePart msg : asAdmin(server, "list-jdbc-connection-pools", null)) {
+            log.log(Level.INFO, "Connection pool: {0}", msg.getMessage());
+        }
+
+        
         params = new ParameterMap();
         params.add("connectionpoolid", "cpMySQLWebAlbumsTest");
         params.add("enabled", "true");
-        params.add("", "jdbc/mysqlWebAlbumsTest");
+        params.add("DEFAULT", "jdbc/mysqlWebAlbumsTest");
         asAdmin(server, "create-jdbc-resource", params);
+
+        for (MessagePart msg : asAdmin(server, "list-jdbc-resources", null)) {
+            log.log(Level.INFO, "JDBC connection : {0}", msg.getMessage());
+        }
     }
 
-    private static void createJDBC_add_Resournces(Server server) {
+    private static void createJDBC_add_Resournces(Server server) throws Throwable {
         ParameterMap params = new ParameterMap();
         params.add("", PATH_RES + "sun-resources.xml");
         asAdmin(server, "add-resources", params);
