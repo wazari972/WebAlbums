2826ad6d49e799e99f74a914be12094e13d526d1
wazari972 <wazari972@373d8ef0-abc8-4151-9680-7fa2cb6b1372>
2010-07-18 11:23:51 +0000

    embedded GF almost working
    git-svn-id: http://subversion.assembla.com/svn/WebAlbums@190 373d8ef0-abc8-4151-9680-7fa2cb6b1372

--- WebAlbums3/trunk/WebAlbums3-Bootstrap/src/net/wazari/bootstrap/GF.java
+++ WebAlbums3/trunk/WebAlbums3-Bootstrap/src/net/wazari/bootstrap/GF.java
@@ -13,17 +13,31 @@ package net.wazari.bootstrap;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.BindException;
 import java.net.ServerSocket;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import net.wazari.bootstrap.GF.Config.User;
 import org.glassfish.api.ActionReport;
 import org.glassfish.api.ActionReport.MessagePart;
 import org.glassfish.api.admin.CommandRunner;
 import org.glassfish.api.admin.ParameterMap;
+import org.glassfish.api.deployment.DeployCommandParameters;
 import org.glassfish.api.embedded.ContainerBuilder;
 import org.glassfish.api.embedded.EmbeddedDeployer;
+import org.glassfish.api.embedded.EmbeddedFileSystem;
 import org.glassfish.api.embedded.LifecycleException;
 import org.glassfish.api.embedded.Server;
 
@@ -32,68 +46,101 @@ import org.glassfish.api.embedded.Server;
  * @author pk033
  */
 public class GF {
-    private static final String PATH_RES = "/Users/kevinpouget/Assembla/WebAlbums3/trunk/WebAlbums3-DAO-JPABeans/setup/" ;
-    private static final String PATH_EAR = "." ;
-    private static final int PORT = 8081 ;
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws LifecycleException, IOException, InterruptedException, Throwable {
-        log.warning("Starting WebAlbums GF bootstrap") ;
+        long timeStart = System.currentTimeMillis() ;
+        log.warning("Starting WebAlbums GF bootstrap");
 
-        File ear =  new File(PATH_EAR, "WebAlbums3-ea.ear") ;
-        if (!ear.isFile()) {
-            log.log(Level.SEVERE, "Impossible to open the ear file at {0}", ear.getPath());
-            return ;
-        }
-
-        File keyfile = new File("keyfile") ;
+        Config cfg = Config.load();
+        
+        log.info(Config.print(cfg)) ;
+        File keyfile = new File("keyfile");
         if (keyfile.exists()) {
-            log.warning("delete ./keyfile ") ;
+            log.warning("delete ./keyfile ");
             keyfile.delete();
         }
 
+        File earfile = new File(cfg.webAlbumsEAR);
+        if (!earfile.exists()) {
+            log.log(Level.WARNING, "The earFile {0} doesn't exist ...", cfg.webAlbumsEAR);
+            return;
+        }
+        log.log(Level.WARNING, "Using EAR: {0}", earfile);
+
         try {
-            new ServerSocket(PORT).close();
-            new ServerSocket(PORT+1).close();
+            new ServerSocket(cfg.port).close();
+            new ServerSocket(cfg.port + 1).close();
         } catch (BindException e) {
-            log.log(Level.WARNING, "Port {0} or {1} already in use", new Object[]{PORT, PORT + 1}) ;
-            return ;
+            log.log(Level.WARNING, "Port {0} or {1} already in use", new Object[]{cfg.port, cfg.port + 1});
+            return;
         }
 
-        Server server = startServer(8081) ;
-        
-
+        Server server = null;
+        EmbeddedDeployer deployer = null;
         try {
-            createUsers(server);
-            createJDBC(server);
+            File installDirGF = new File(cfg.glassfishDIR);
+            installDirGF = installDirGF.getCanonicalFile();
+            log.log(Level.WARNING, "Using Glassfish FS: {0}", installDirGF);
+            
 
-            EmbeddedDeployer deployer = server.getDeployer();
+            EmbeddedFileSystem.Builder efsb = new EmbeddedFileSystem.Builder();
+            efsb.autoDelete(false);
+            efsb.installRoot(installDirGF, true);
+
+            /*EAR not recognized with domain ...*/
+            //File instanceGF = new File(cfg.glassfishDIR+"/domains/domain1");
+            //efsb.instanceRoot(instanceGF) ;
+            EmbeddedFileSystem efs = efsb.build();
+
+            server = startServer(cfg.port, efs);
+
+            for (User usr : cfg.user) {
+                createUsers(server, usr);
+            }
+            
+            createJDBC_add_Resources(server, cfg.sunResourcesXML);
+
+            deployer = server.getDeployer();
             log.info("Deploying ");
-            String appName = null ;
-            appName = deployer.deploy(new File(PATH_EAR, "WebAlbums3-ea.ear"), null);
+            String appName = null;
+            DeployCommandParameters params = new DeployCommandParameters();
+            appName = deployer.deploy(new File(cfg.webAlbumsEAR), params);
             log.log(Level.INFO, "Deployed {0}", appName);
-            
-            log.log(Level.INFO, "Ready to server at http://localhost:{0}/WebAlbums3-Servlet", Integer.toString(PORT));
-            log.log(Level.INFO, "Connect to http://localhost:{0} to shutdown the server", Integer.toString(PORT+1));
 
-            ServerSocket serv = new ServerSocket(PORT+1) ;
-            serv.accept().close() ;
-            serv.close() ;
-            
-            server.stop();
+            long loadingTime = System.currentTimeMillis();
+            float time = ((float) (loadingTime - timeStart) / 1000);
+
+            log.log(Level.INFO, "Ready to server at http://localhost:{0}/WebAlbums3-Servlet after {1}s", new Object[] {Integer.toString(cfg.port), time});
+            log.log(Level.INFO, "Connect to http://localhost:{0} to shutdown the server", Integer.toString(cfg.port + 1));
+
+            ServerSocket servSocker = new ServerSocket(cfg.port + 1);
+            servSocker.accept().close();
+            servSocker.close();
+
         } finally {
-            server.stop();
+            if (deployer != null) {
+                deployer.undeployAll();
+            }
+            if (server != null) {
+                server.stop();
+            }
+
             if (keyfile.exists()) {
-                log.warning("delete ./keyfile ") ;
+                log.warning("delete ./keyfile ");
                 keyfile.delete();
             }
         }
     }
 
-    private static Server startServer(int port) throws LifecycleException, IOException {
-        Server.Builder builder = new Server.Builder("WebAlbums");
+    private static Server startServer(int port, EmbeddedFileSystem efs) throws LifecycleException, IOException {
+        Server.Builder builder = new Server.Builder("test");
+        if (efs != null) {
+            builder.embeddedFileSystem(efs);
+        }
+        builder.logger(true);
         Server server = builder.build();
         server.addContainer(ContainerBuilder.Type.all);
         server.start();
@@ -101,14 +148,14 @@ public class GF {
         // Specify the port
         server.createPort(port);
 
-        return server ;
+        return server;
 
     }
 
     private static List<MessagePart> asAdmin(Server server, String command, ParameterMap params) throws Throwable {
         CommandRunner runner = server.getHabitat().getComponent(CommandRunner.class);
         ActionReport report = server.getHabitat().getComponent(ActionReport.class);
-        log.log(Level.INFO, "Invok {0} {1}", new Object[]{command, params});
+        log.log(Level.INFO, "Invoke {0} {1}", new Object[]{command, params});
 
         log.log(Level.INFO, "command \"{0}\" invoked", command);
         if (params == null) {
@@ -117,77 +164,115 @@ public class GF {
             runner.getCommandInvocation(command, report).parameters(params).execute();
         }
         log.log(Level.INFO, "command finished with {0}", report.getActionExitCode());
-        
-        if (report.hasFailures()){
+
+        if (report.hasFailures()) {
             if (report.getFailureCause() != null) {
                 throw report.getFailureCause();
             } else {
-                throw new Exception (report.getMessage()) ;
+                throw new Exception(report.getMessage());
             }
         }
-        log.log(Level.INFO, "command finished with {0}", report.getActionExitCode());
-        return report.getTopMessagePart().getChildren() ;
+
+        return report.getTopMessagePart().getChildren();
     }
 
-    private static void createUsers(Server server) {
+    private static void createUsers(Server server, User usr) throws Throwable {
         ParameterMap params = new ParameterMap();
-        params.add("groups", "Admin");
-        params.add("groups", "Manager");
-        params.add("username", "Kevin");
-        params.add("userpassword", "pass");
-        try {
-            asAdmin(server, "create-file-user", params);
-        } catch (Throwable t){
-            //t.printStackTrace();
+
+        params.add("username", usr.name);
+        params.add("userpassword", usr.password);
+        for (String group :usr.groups) {
+            params.add("groups", group);
         }
+        
+        asAdmin(server, "create-file-user", params);
     }
 
-    private static void createJDBC(Server server) throws Throwable {
+    private static void createJDBC_add_Resources(Server server, String path) throws Throwable {
+        if (!new File(path).exists()) throw new IllegalArgumentException(path + " doesn't exists") ;
         ParameterMap params = new ParameterMap();
-        params.add("datasourceclassname", "com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
-        params.add("restype", "javax.sql.DataSource");
-        params.add("isisolationguaranteed", "true");
-        params.add("isconnectvalidatereq", "false");
-        params.add("validationmethod", "auto-commit");
-        params.add("failconnection", "false");
-        params.add("allownoncomponentcallers", "false");
-        params.add("nontransactionalconnections", "false");
-        params.add("leakreclaim", "false");
-        params.add("lazyconnectionenlistment", "false");
-        params.add("lazyconnectionassociation", "false");
-        params.add("associatewiththread", "false");
-        params.add("matchconnections", "false");
-        params.add("ping", "true");
-        params.add("pooling", "true");
-
-        params.add("wrapjdbcobjects", "false");
-
-        params.add("property", "URL=jdbc\\:mysql\\://127.0.0.1/WebAlbums_test:User=wazari972:Password=ijaheb");
-        params.add("DEFAULT", "cpMySQLWebAlbumsTest");
-        asAdmin(server, "create-jdbc-connection-pool", params);
-
-
-        for (MessagePart msg : asAdmin(server, "list-jdbc-connection-pools", null)) {
-            log.log(Level.INFO, "Connection pool: {0}", msg.getMessage());
-        }
-
-
-        params = new ParameterMap();
-        params.add("connectionpoolid", "cpMySQLWebAlbumsTest");
-        params.add("enabled", "true");
-        params.add("DEFAULT", "jdbc/mysqlWebAlbumsTest");
-        asAdmin(server, "create-jdbc-resource", params);
-
-        for (MessagePart msg : asAdmin(server, "list-jdbc-resources", null)) {
-            log.log(Level.INFO, "JDBC connection : {0}", msg.getMessage());
-        }
-    }
-
-    private static void createJDBC_add_Resournces(Server server) throws Throwable {
-        ParameterMap params = new ParameterMap();
-        params.add("", PATH_RES + "sun-resources.xml");
+        params.add("", path );
         asAdmin(server, "add-resources", params);
     }
-
     private static final Logger log = Logger.getLogger(GF.class.getName());
+
+    @XmlRootElement
+
+    static class Config {
+
+        public static Config loadDefault(File cfgFile) throws JAXBException {
+            Config cfg = new Config();
+            cfg.sunResourcesXML = "conf/sun-resources.xml" ;
+            cfg.glassfishDIR = "./glassfish" ;
+            cfg.webAlbumsEAR = "bin/WebAlbums3-ea.ear" ;
+            cfg.port = 8080 ;
+            cfg.user = new LinkedList<User>() ;
+
+            User usr = new User() ;
+            usr.name = "Kevin" ;
+            usr.password = "" ;
+            usr.groups = Arrays.asList(new String[]{"Admin", "Manager"}) ;
+            cfg.user.add(usr) ;
+
+            if (!cfgFile.exists()) {
+                save(cfgFile, cfg);
+            }
+            
+            return cfg ;
+        }
+
+        public static Config load() throws JAXBException {
+            File file = new File("conf/config.xml");
+            if (!file.isFile()) {
+                return loadDefault(file) ;
+            }
+
+            //Create JAXB Context
+            JAXBContext jc = JAXBContext.newInstance(Config.class);
+            Unmarshaller um = jc.createUnmarshaller();
+            Config cfg = (Config) um.unmarshal(file);
+
+            return cfg;
+        }
+
+        public static <T> String print(T xml) throws JAXBException {
+            //Create JAXB Context
+            JAXBContext jc = JAXBContext.newInstance(Config.class);
+
+            //Create marshaller
+            Marshaller marshaller = jc.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            StringWriter writer = new StringWriter();
+            marshaller.marshal(xml, writer);
+
+            return writer.toString();
+        }
+
+        public static <T> void save(File file, T obj) throws JAXBException {
+            //Create JAXB Context
+            JAXBContext jc = JAXBContext.newInstance(Config.class);
+            //Create marshaller
+            Marshaller marshaller = jc.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            marshaller.marshal(obj, file);
+        }
+        @XmlAttribute
+        int port ;
+        @XmlElement
+        String sunResourcesXML ;
+        @XmlElement
+        String webAlbumsEAR ;
+        @XmlElement
+        String glassfishDIR ;
+        @XmlElementWrapper(name="users")
+        List<User> user ;
+
+        static class User {
+            @XmlAttribute String name ;
+            @XmlAttribute String password ;
+            @XmlElement List<String> groups ;
+        }
+    }
 }
