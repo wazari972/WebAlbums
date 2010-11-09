fc6de0aa45a103279b025073b83841f5c5f91e8f
wazari972 <wazari972@373d8ef0-abc8-4151-9680-7fa2cb6b1372>
2010-11-09 21:51:03 +0000

    looking for the metamodel bug
    git-svn-id: http://subversion.assembla.com/svn/WebAlbums@295 373d8ef0-abc8-4151-9680-7fa2cb6b1372

--- WebAlbums3/trunk/EJBModule1/src/java/net/wazari/dao/jpa/entity/JPAUtilisateur.java
+++ WebAlbums3/trunk/EJBModule1/src/java/net/wazari/dao/jpa/entity/JPAUtilisateur.java
@@ -14,8 +14,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -26,7 +24,6 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
-import org.hibernate.annotations.GenericGenerator;
 
 /**
  *
