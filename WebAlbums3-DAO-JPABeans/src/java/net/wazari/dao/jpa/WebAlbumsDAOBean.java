908f02cfb0bcb90d051c710a31477b630bb5a8d2
Kevin P <wazari972@gmail.com>
2012-05-03 21:14:23 +0200

    back to PRODUCTION database

--- WebAlbums3-DAO-JPABeans/src/java/net/wazari/dao/jpa/WebAlbumsDAOBean.java
+++ WebAlbums3-DAO-JPABeans/src/java/net/wazari/dao/jpa/WebAlbumsDAOBean.java
@@ -46,7 +46,7 @@ public class WebAlbumsDAOBean {
 
     public static final String PERSISTENCE_UNIT_Prod = PERSISTENCE_UNIT_MySQL_Prod ;
     
-    public static final String PERSISTENCE_UNIT = PERSISTENCE_UNIT_MySQL_Simu ;
+    public static final String PERSISTENCE_UNIT = PERSISTENCE_UNIT_Prod ;
 
     @PersistenceContext(unitName=WebAlbumsDAOBean.PERSISTENCE_UNIT)
     private EntityManager em;
