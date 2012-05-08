b19ec7dea467eaeb556ea60c7504bc7750575433
Kevin P <wazari972@gmail.com>
2012-05-08 09:51:11 +0200

    fix refresh bug in CHOIX/GPX

--- WebAlbums3-Servlet/web/static/scripts/Choix.js
+++ WebAlbums3-Servlet/web/static/scripts/Choix.js
@@ -30,13 +30,17 @@ function createGpxesMap() {
     var map = loadMap("gpxChoix");
     $("#gpxChoix").data("map", map)
     var bounds = null;
-    $(".gpxTrack").each(function () {
+    var last_index = $(".gpxTrack").size() - 1
+    
+    $(".gpxTrack").each(function (index) {
         var do_extend = function (mmap, llayer) {
             if (bounds == null) {
                 bounds = llayer.getDataExtent()
             } else
                 bounds.extend(llayer.getDataExtent())
-            map.zoomToExtent(bounds, false);
+            
+            if (index == last_index)
+                map.zoomToExtent(bounds, false);
         }
         var layer = init_gpx_layer(map, $(this).text(), $(this).attr("rel"), do_extend)
         $(this).data("layer", layer)
