23c1731692e1f7346cfce99a92345777dd512674
Kevin P <wazari972@gmail.com>
2012-06-28 20:37:37 +0200

    fix

--- WebAlbums3-Downloader/download_a_theme.py
+++ WebAlbums3-Downloader/download_a_theme.py
@@ -2,10 +2,14 @@
 
 import tools
     
+tools.ABS_DATA_PATH = "../"
+    
 tools.login("kevin", "")
-
+exit()
+tools.get_a_theme(5, "Martinique")
+theme = None
 tools.get_a_theme(4, "Vayrac")
 theme = None
-tools.get_a_theme(5, "Martinique")
+tools.get_a_theme(11, "Mariage")
 
 tools.print_error_report()
