3c7b9ee5478271c40644a886c170063bfee69d5e
Kevin P <wazari972@gmail.com>
2012-09-06 18:25:11 +0200

    standalone seems to work

--- WebAlbums3/start.sh
+++ WebAlbums3/start.sh
@@ -1,2 +1,11 @@
-WA_HOME=/home/kevin/vayrac/WebAlbums/WebAlbums3
-echo java -Droot.path=/home/kevin/vayrac -Dfile.encoding=UTF-8 -Djava.library.path=$WA_HOME/lib -classpath $WA_HOME/lib/glassfish-embedded-all-3.1.1.jar:$WA_HOME/lib/WebAlbums-libs/RT-DB-mysql-connector-java-5.1.12-bin.jar:$WA_HOME/lib/WebAlbums-libs/RT-Log-logback-classic-0.9.24.jar:$WA_HOME/lib/WebAlbums-libs/RT-Log-logback-core-0.9.24.jar:$WA_HOME/bin/WebAlbums3-Bootstrap.jar net.wazari.bootstrap.GF
+ROOT_PATH=/other/Web
+WA_HOME="."
+java -Droot.path=$ROOT_PATH \
+     -Dfile.encoding=UTF-8 \
+	 -Djava.library.path=$WA_HOME/lib \
+	 -classpath \
+$WA_HOME/lib/WebAlbums-libs/RT-DB-mysql-connector-java-5.1.12-bin.jar:\
+$WA_HOME/lib/WebAlbums-libs/RT-Log-logback-classic-0.9.24.jar:\
+$WA_HOME/lib/WebAlbums-libs/RT-Log-logback-core-0.9.24.jar:\
+$WA_HOME/lib/WebAlbums-libs/CP-Log-slf4j-api-1.6.0.jar:\
+$WA_HOME/bin/WebAlbums3-Bootstrap.jar net.wazari.bootstrap.GF
