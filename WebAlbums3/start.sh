c26f45e80b83334453ba5b9dcefa7e862becfd6e
Kevin P <wazari972@gmail.com>
2012-09-06 19:11:48 +0200

    STANDALONE: ... but some is still required

--- WebAlbums3/start.sh
+++ WebAlbums3/start.sh
@@ -3,9 +3,4 @@ WA_HOME="."
 java -Droot.path=$ROOT_PATH \
      -Dfile.encoding=UTF-8 \
 	 -Djava.library.path=$WA_HOME/lib \
-	 -classpath \
-$WA_HOME/lib/WebAlbums-libs/RT-DB-mysql-connector-java-5.1.12-bin.jar:\
-$WA_HOME/lib/WebAlbums-libs/RT-Log-logback-classic-0.9.24.jar:\
-$WA_HOME/lib/WebAlbums-libs/RT-Log-logback-core-0.9.24.jar:\
-$WA_HOME/lib/WebAlbums-libs/CP-Log-slf4j-api-1.6.0.jar:\
-$WA_HOME/bin/WebAlbums3-Bootstrap.jar net.wazari.bootstrap.GF
+	 -jar $WA_HOME/dist/WebAlbums3-Bootstrap.jar
