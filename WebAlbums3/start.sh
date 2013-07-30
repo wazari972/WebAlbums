c423e68f6ae38ca26c3efdc10cfd2bab824f50ff
Kevin Pouget <webalbums@kevin.pouget.me>
2013-07-30 18:47:52 +0200

    fix dependency problem, missing library

--- WebAlbums3/start.sh
+++ WebAlbums3/start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+
 #change root.path as you need
 # then git update-index --assume-unchanged WebAlbums3/root.path
 ROOT_PATH=$(cat root.path)
 WA_HOME="."
-java -Droot.path=$ROOT_PATH \
-     -Dfile.encoding=UTF-8 \
-	 -Djava.library.path=$WA_HOME/lib \
-	 -jar $WA_HOME/dist/WebAlbums3-Bootstrap.jar
+java -cp $WA_HOME/lib/cglib-nodep-2.2.jar -Droot.path=$ROOT_PATH \
+	-Dfile.encoding=UTF-8 \
+	-Djava.library.path=$WA_HOME/lib \
+	-jar $WA_HOME/dist/WebAlbums3-Bootstrap.jar
