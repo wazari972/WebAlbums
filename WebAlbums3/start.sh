9e9daa8562916ab7b60e53c7135315a4aef952e5
Kevin Pouget <webalbums@kevin.pouget.me>
2013-07-30 21:50:06 +0200

    fix

--- WebAlbums3/start.sh
+++ WebAlbums3/start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-#change root.path as you need
-# then git update-index --assume-unchanged WebAlbums3/root.path
-ROOT_PATH=$(cat root.path)
+# Change this to the actual location of WebAlbum s
 WA_HOME="."
-java -cp $WA_HOME/lib/cglib-nodep-2.2.jar -Droot.path=$ROOT_PATH \
-	-Dfile.encoding=UTF-8 \
+java    -Dfile.encoding=UTF-8 \
 	-Djava.library.path=$WA_HOME/lib \
 	-jar $WA_HOME/dist/WebAlbums3-Bootstrap.jar $*
