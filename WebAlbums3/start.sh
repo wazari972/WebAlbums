e7e82eddaac3000444b7ca8804607892acf6ef59
Kevin Pouget <webalbums@kevin.pouget.me>
2013-08-01 16:26:36 +0200

    Release of August 1st, 2013 (2)
    Fix start.sh (no more modification required)

--- WebAlbums3/start.sh
+++ WebAlbums3/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Change this to the actual location of WebAlbum s
-WA_HOME="."
+WA_HOME=$(dirname $0)
 cd $WA_HOME
 java    -Dfile.encoding=UTF-8 \
-	-Djava.library.path=$WA_HOME/lib \
-	-jar $WA_HOME/dist/WebAlbums3-Bootstrap.jar $*
+	-Djava.library.path=./lib \
+	-jar ./dist/WebAlbums3-Bootstrap.jar $*
