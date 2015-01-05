78def9facefcaa5cffa7193f7fead28adf77d787
Kevin P <wazari972@gmail.com>
2015-01-05 17:30:22 +0100

    split album list year by year

--- WebAlbums3-FS/WebAlbums3-FS/src/java/net/wazari/view/vfs/entity/Albums.java
+++ WebAlbums3-FS/WebAlbums3-FS/src/java/net/wazari/view/vfs/entity/Albums.java
@@ -4,12 +4,16 @@
  */
 package net.wazari.view.vfs.entity;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import net.wazari.dao.entity.Theme;
 import net.wazari.libvfs.annotation.ADirectory;
 import net.wazari.libvfs.annotation.Directory;
 import net.wazari.libvfs.annotation.File;
+import net.wazari.libvfs.inteface.BasicDirectory;
+import net.wazari.libvfs.inteface.SDirectory;
 import net.wazari.libvfs.inteface.VFSException;
 import net.wazari.service.exception.WebAlbumsServiceException;
 import net.wazari.service.exchange.xml.album.XmlAlbum;
@@ -24,7 +28,8 @@ import net.wazari.view.vfs.Session;
 public class Albums extends Listing implements ADirectory {
     @Directory
     @File
-    public List<Album> albums = new LinkedList<>();
+    public List<BasicDirectory> years = new LinkedList<>();
+    
     private final Theme theme;
     private final Launch aThis;
     private final Root root;
@@ -44,8 +49,19 @@ public class Albums extends Listing implements ADirectory {
             throw new VFSException(ex);
         }
         
+        Map<String, BasicDirectory> yearalbums = new HashMap<>();
         for (XmlAlbum album : entries.album) {
-            albums.add(new Album(this.root, album.date, album.name, album.id, theme, aThis)) ;
+            String key = album.date.year;
+            BasicDirectory year;
+            if (!yearalbums.containsKey(key)) {
+                year = new BasicDirectory(key);
+                yearalbums.put(key, year);
+                years.add(year);
+            } else {
+                year = yearalbums.get(key);
+            }
+            
+            year.addDirInside(new Album(this.root, album.date, album.name, album.id, theme, aThis)) ;
         }
-    }
+    }    
 }
