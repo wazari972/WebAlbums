6a81cfe59b2dfc76f548f132a5902222779a562a
Kevin P <wazari972@gmail.com>
2015-01-12 19:53:44 +0100

    work on packaging

---
+++ package/link.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# http://stackoverflow.com/a/5027832/341106
+# both $1 and $2 are absolute paths
+# returns $2 relative to $1
+
+target=$1
+source=$2
+
+target=$1
+common_part=$(dirname $source)
+back=
+while [ "${target#$common_part}" = "${target}" ]; do
+  common_part=$(dirname $common_part)
+  back="../${back}"
+done
+
+good_target=${back}${target#$common_part/}
+
+cmd="ln -s $good_target $source"
+echo "  $cmd"
+$cmd
--- package/setup_data_dir.sh
+++ package/setup_data_dir.sh
@@ -1,8 +1,18 @@
 #! /bin/sh
 
+if [[ "$*" == *"--mkdirs"* ]]
+then
+    DO_MKDIRS=1
+    shift
+fi
+if [[ "$*" == *"--configure"* ]]
+then
+    DO_CONFIGURE=1
+    shift
+fi
+
 ROOT_PATH=$1
 CONFIG_FILE=$2
-
 TMP=$3
 
 IMAGES=images
@@ -11,11 +21,22 @@ FTP=ftp
 BACKUP=backup
 PLUGINS=plugins
 
-sed -i "s|IMAGES|$IMAGES|g" $CONFIG_FILE
-sed -i "s|MINIS|$MINIS|g" $CONFIG_FILE
-sed -i "s|TMP|$TMP|g" $CONFIG_FILE
-sed -i "s|FTP|$FTP|g" $CONFIG_FILE
-sed -i "s|BACKUP|$BACKUP|g" $CONFIG_FILE
-sed -i "s|PLUGINS|$PLUGINS|g" $CONFIG_FILE
-
-mkdir -p $ROOT_PATH/$IMAGES $ROOT_PATH/$MINIS $ROOT_PATH/$BACKUP $ROOT_PATH/$FTP $ROOT_PATH/$PLUGINS
+if [ "$DO_CONFIGURE" == 1 ]
+then
+    sed -i "s|ROOT_PATH|$ROOT_PATH|g" $CONFIG_FILE
+    sed -i "s|IMAGES|$IMAGES|g" $CONFIG_FILE
+    sed -i "s|MINIS|$MINIS|g" $CONFIG_FILE
+    sed -i "s|TMP|$TMP|g" $CONFIG_FILE
+    sed -i "s|FTP|$FTP|g" $CONFIG_FILE
+    sed -i "s|BACKUP|$BACKUP|g" $CONFIG_FILE
+    sed -i "s|PLUGINS|$PLUGINS|g" $CONFIG_FILE
+fi
+if [ "$DO_MKDIRS" == 1 ]
+then
+    mkdir $ROOT_PATH/$IMAGES \
+          $ROOT_PATH/$MINIS \
+          $ROOT_PATH/$BACKUP \
+          $ROOT_PATH/$FTP \
+          $ROOT_PATH/$PLUGINS \
+          -p
+fi
